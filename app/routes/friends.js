02699b3bd32205d4f2d916b6f870e476eb6a1a5e
Brian Crosby <bcrosby@LT-21069.local>
2015-09-29 09:15:01 -0400

    Added ability to delete friends. Refactored code

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  actions: {
+    save() {
+      console.log("Bubbled up from route");
+      return true;
+    },
+    cancel() {
+      console.log("Bubbled up from route");
+      return true;
+    },
+    delete: function(friend) {
+      friend.destroyRecord().then(() => {
+        this.transitionToRoute('friends.index');
+      });
+    }
+  }
 });
